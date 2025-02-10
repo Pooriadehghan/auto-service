d03aab9b917de144cb386f0ecd0a456b73d58fd0
pooria <pouriyadehghan81@gmail.com>
2025-02-10 23:45:10 +0330

    validation

---
+++ model/tools/logging.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+
+class Logger:
+    logging.baseicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(name)s %(levelname)5s %(message)s',
+        encoding="UTF-8",
+        handlers=[
+
+            logging.streamhandler(sys.stdout),
+        ]
+    )
+    @classmethod
+    def info(cls, message):
+        logging.info(message)
+
+
+    @classmethod
+    def error(cls, message):
+        logging.error(message)
